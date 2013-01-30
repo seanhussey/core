ffb9b572351d906862afa112650b268849d5c068
Abdul Rauf <abdul@Abdul-Raufs-MacBook-Pro.local>
2013-01-30 14:27:59 +1030

    Whitelist issue fixed for comments

--- app/controllers/gluttonberg/public/comments_controller.rb
+++ app/controllers/gluttonberg/public/comments_controller.rb
@@ -1,11 +1,12 @@
 module Gluttonberg
   module Public
     class CommentsController <  Gluttonberg::Public::BaseController
-  
+
       def create
         @blog = Gluttonberg::Blog.first(:conditions => {:slug => params[:blog_id]})
         @article = Gluttonberg::Article.first(:conditions => {:slug => params[:article_id], :blog_id => @blog.id})
-        @comment = @article.comments.new(params[:comment].merge(:blog_slug => params[:blog_id]))
+        @comment = @article.comments.new(params[:comment])
+        @comment.blog_slug = params[:blog_id]
         @comment.author_id = current_member.id if current_member
         if @comment.save
           if Setting.get_setting("comment_notification") == "Yes" || @blog.moderation_required == true
@@ -13,16 +14,16 @@ module Gluttonberg
               Notifier.comment_notification_for_admin(user , @article , @comment).deliver
             end
           end
-          
+
           @subscription = CommentSubscription.find(:first , :conditions => {:article_id => @article.id , :author_email => @comment.writer_email })
           if @comment.subscribe_to_comments == "1" && @subscription.blank?
             @subscription = CommentSubscription.create( {:article_id => @article.id , :author_email => @comment.writer_email , :author_name => @comment.writer_name } )
           elsif (@comment.subscribe_to_comments.blank? || @comment.subscribe_to_comments == "0")  && !@subscription.blank?
             #unsubscribe
-            @subscription.destroy 
+            @subscription.destroy
           end
         else
-          
+
         end
         if Gluttonberg.localized?
           redirect_to blog_article_path(current_localization_slug , @blog.slug, @article.slug)
@@ -30,8 +31,8 @@ module Gluttonberg
           redirect_to blog_article_path(:blog_id =>  @blog.slug, :id => @article.slug)
         end
       end
-  
-      private 
+
+      private
         def current_localization_slug
            if @locale
              @locale.slug
@@ -40,6 +41,6 @@ module Gluttonberg
            end
         end
     end
-    
+
   end
 end
