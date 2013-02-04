a1c4f21f9cd157085cf21cb9c887d686aef0f43a
Abdul Rauf <abdul@Abdul-Raufs-MacBook-Pro.local>
2013-02-04 16:16:57 +1030

    Check blacklisted words in author_email, author_name, author_website

--- app/models/gluttonberg/comment.rb
+++ app/models/gluttonberg/comment.rb
@@ -122,6 +122,16 @@ module Gluttonberg
           dspam = Gluttonberg::Content::Despamilator.new(self.body)
           self.spam = (dspam.score >= 1.0)
           self.spam_score = dspam.score
+          unless self.spam
+            naughty_word_parser = Gluttonberg::Content::DespamilatorFilter::NaughtyWords.new
+            if !self.author_email.blank? && naughty_word_parser.local_parse(self.author_email) >= 1.0
+              self.spam = true
+            elsif !self.author_name.blank? && naughty_word_parser.local_parse(self.author_name) >= 1.0
+              self.spam = true
+            elsif !self.author_website.blank? && naughty_word_parser.local_parse(self.author_website) >= 1.0
+              self.spam = true
+            end
+          end
         else
           self.spam = true
           self.spam_score = 1.0
--- lib/gluttonberg/content/despamilator/filter/naughty_words.rb
+++ lib/gluttonberg/content/despamilator/filter/naughty_words.rb
@@ -25,24 +25,26 @@ module Gluttonberg
           unless gb_blacklist_settings.blank?
             gb_blacklist_settings_words = gb_blacklist_settings.split(",")
             gb_blacklist_settings_words.each do |word|
-              subject.register_match!({:score => 1.0, :filter => self}) if text =~ /\b#{word}s?\b/
+              subject.register_match!({:score => 1.0, :filter => self}) if text =~ /\b#{word.strip.downcase}s?\b/
             end
           end
         end
 
         def local_parse subject
           local_score = 0.0
-          text = subject.downcase
+          unless subject.blank?
+            text = subject.downcase
 
-          naughty_words.each do |word|
-            local_score += 0.1 if text =~ /\b#{word}s?\b/
-          end
+            naughty_words.each do |word|
+              local_score += 0.1 if text =~ /\b#{word}s?\b/
+            end
 
-          gb_blacklist_settings = Gluttonberg::Setting.get_setting("comment_blacklist")
-          unless gb_blacklist_settings.blank?
-            gb_blacklist_settings_words = gb_blacklist_settings.split(",")
-            gb_blacklist_settings_words.each do |word|
-              local_score += 1.0 if text =~ /\b#{word}s?\b/
+            gb_blacklist_settings = Gluttonberg::Setting.get_setting("comment_blacklist")
+            unless gb_blacklist_settings.blank?
+              gb_blacklist_settings_words = gb_blacklist_settings.split(",")
+              gb_blacklist_settings_words.each do |word|
+                local_score += 1.0 if text.include?(word.strip.downcase)
+              end
             end
           end
           local_score
