7acb715ebe72122317393abcd844c61a09e5c489
Abdul Rauf <abdul@Abdul-Raufs-MacBook-Pro.local>
2013-06-19 14:30:39 +0930

    AssetCategory model improvements
    Indentation fixes
    method_missing better implementation. now it will throw NoMethodError exception {category_name}_category method call is not valid.
    
    use of .where instead of find

--- app/models/gluttonberg/asset_category.rb
+++ app/models/gluttonberg/asset_category.rb
@@ -1,38 +1,40 @@
 module Gluttonberg
   class AssetCategory < ActiveRecord::Base
-      self.table_name = "gb_asset_categories"
-      has_many :asset_types , :class_name => "AssetType"
-      has_many :assets, :through => :asset_types
+    self.table_name = "gb_asset_categories"
+    has_many :asset_types , :class_name => "AssetType"
+    has_many :assets, :through => :asset_types
 
-      validates_uniqueness_of :name
-      validates_presence_of :name
-      attr_accessible :name , :unknown
+    validates_uniqueness_of :name
+    validates_presence_of :name
+    attr_accessible :name , :unknown
 
-      def self.method_missing(methId, *args)
-        method_info = methId.id2name.split('_')
-        if method_info.length == 2 then
-          if method_info[1] == 'category' then
-            cat_name = method_info[0]
-            if cat_name then
-              return find(:first , :conditions => "name = '#{cat_name}'")
-            end
+    def self.method_missing(methId, *args)
+      method_info = methId.id2name.split('_')
+      if method_info.length == 2 then
+        if method_info[1] == 'category' then
+          cat_name = method_info[0]
+          if cat_name then
+            cat = where(:name => cat_name).first
+            return cat unless cat.blank?
           end
         end
       end
+      raise NoMethodError
+    end
 
-      def self.build_defaults
-        # Ensure the default categories exist in the database.
-        ensure_exists('audio', false)
-        ensure_exists('image', false)
-        ensure_exists('video', false)
-        ensure_exists('document', false)
-        ensure_exists(Library::UNCATEGORISED_CATEGORY, true)
-      end
+    def self.build_defaults
+      # Ensure the default categories exist in the database.
+      ensure_exists('audio', false)
+      ensure_exists('image', false)
+      ensure_exists('video', false)
+      ensure_exists('document', false)
+      ensure_exists(Library::UNCATEGORISED_CATEGORY, true)
+    end
 
-      private
+    private
 
       def self.ensure_exists(name, unknown)
-        cat = find(:first , :conditions => "name = '#{name}'")
+        cat = where(:name => name).first
         if cat then
           cat.unknown = unknown
           cat.save
@@ -40,6 +42,5 @@ module Gluttonberg
           cat = create(:name => name, :unknown => unknown)
         end
       end
-
   end
 end
