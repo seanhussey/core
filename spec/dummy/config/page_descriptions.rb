83df5c5076ee3974fc6096ff68ea9a7d3363ea2b
Abdul Rauf <abdul@Abdul-Raufs-MacBook-Pro.local>
2013-06-20 14:40:40 +0930

    dummy app page_description.rb

---
+++ spec/dummy/config/page_descriptions.rb
@@ -0,0 +1,65 @@
+Gluttonberg::PageDescription.add do
+
+  # home page page description
+  page :home do
+    label "Home"
+    description "Homepage"
+    home true
+    view "home"
+    layout "public"
+  end
+
+  # page description with two three sections.
+  page :newsletter do
+    label "Newsletter"
+    description "Newsletter Page"
+    view "newsletter"
+    layout "public"
+
+    section :title do
+      label "Title"
+      type :plain_text_content
+    end
+
+    section :description do
+      label "Description"
+      type :html_content
+    end
+
+    section :image do
+      label "Image"
+      type  :image_content
+    end
+
+  end
+
+  # page description which redirects to rails defined route examples
+  page :examples do
+    label "Examples"
+    description "Examples Page"
+
+    rewrite_to 'examples'
+    layout "public"
+  end
+
+  # page description with a single content section
+  page :about do
+    label "About"
+    description "About Page"
+    view "about"
+    layout "public"
+    section :top_content do
+      label "Content"
+      type :html_content
+    end
+  end
+
+  # page without any dyanmic section.
+  page :sitemap do
+    label "Site Map"
+    description "Site Map"
+    view "sitemap"
+    layout "public"
+  end
+
+end
