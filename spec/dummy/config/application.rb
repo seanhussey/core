861bb288b642aa89cb10d551311f86bfa19a87c0
Abdul Rauf <abdul@Abdul-Raufs-MacBook-Pro.local>
2013-06-20 13:24:22 +0930

    added few different image sizes for qA

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -13,7 +13,12 @@ module Dummy
      config.localize = false
      # config.enable_members = {:email_verification => true}
      config.thumbnails = {
-       :jwysiwyg_image => {:label => "Thumb for jwysiwyg", :filename => "_jwysiwyg_image", :geometry => "250x200"}
+       :jwysiwyg_image => {:label => "Thumb for jwysiwyg", :filename => "_jwysiwyg_image", :geometry => "250x200"},
+       :fixed_image => {:label => "Fixed Size Image", :filename => "fixed_image", :geometry => "1000x1000#"},
+       :fixed_width_image => {:label => "Fixed width image", :filename => "fixed_width_image", :geometry => "400"},
+       :fixed_height_image => {:label => "Fixed height image", :filename => "fixed_height_image", :geometry => "x500"},
+       :max_width_image => {:label => "Max width image", :filename => "max_width_image", :geometry => "400>"},
+       :max_height_image => {:label => "Max height image", :filename => "max_height_image", :geometry => "x500>"}
      }
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
