b8cadddff3489aadd836308dde9e2deb5df1d55c
Abdul Rauf <abdul@Abdul-Raufs-MacBook-Pro.local>
2013-06-21 16:09:32 +0930

    improvement

--- app/models/gluttonberg/feed.rb
+++ app/models/gluttonberg/feed.rb
@@ -3,10 +3,10 @@ module Gluttonberg
     belongs_to :user
     belongs_to :feedable, :polymorphic => true
     self.table_name = "gb_feeds"
-    attr_accessible :user, :feedable_type, :feedable_id, :title, :action_type
+    attr_accessible :user, :feedable, :feedable_type, :feedable_id, :title, :action_type
 
     def self.log(user,object,title,action_type)
-      self.create(:user => user , :feedable_type => object.class.to_s,  :feedable_id => object.id, :title => title , :action_type => action_type)
+      self.create(:user => user , :feedable => object, :title => title , :action_type => action_type)
     end
 
 
