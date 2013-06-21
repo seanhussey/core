bb07317f4fb1bac8f120378d156fbc081c0b64b6
Abdul Rauf <abdul@Abdul-Raufs-MacBook-Pro.local>
2013-06-21 16:17:41 +0930

    no need for membership main index

--- app/controllers/gluttonberg/admin/membership/main_controller.rb
+++ app/controllers/gluttonberg/admin/membership/main_controller.rb
@@ -1,11 +1,12 @@
 
 module Gluttonberg
   module Admin
-    module Membership  
+    module Membership
           class MainController < Gluttonberg::Admin::Membership::BaseController
             def index
+              redirect_to admin_membership_members_path
             end
           end
-      end    
+      end
   end
 end
