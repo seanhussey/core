2f463ad5bf023acbfad009d77eba8f03319a59ef
Abdul Rauf <abdul@Abdul-Raufs-MacBook-Pro.local>
2013-06-21 16:09:19 +0930

    find to where

--- app/models/gluttonberg/comment_subscription.rb
+++ app/models/gluttonberg/comment_subscription.rb
@@ -8,7 +8,7 @@ module Gluttonberg
     attr_accessible :article_id , :author_email , :author_name
 
     def self.notify_subscribers_of(article , comment)
-      subscribers = self.find(:all , :conditions => {:article_id => article.id})
+      subscribers = self.where(:article_id => article.id).all
       subscribers.each do |subscriber|
         unless subscriber.author_email == comment.writer_email
           Notifier.delay.comment_notification(subscriber , article , comment ) #.deliver # its using delayed job but i am setting sent time immediately
