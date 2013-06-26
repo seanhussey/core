2fca635bee254501a06d45af5417be6202668bbd
Abdul Rauf <abdul@Abdul-Raufs-MacBook-Pro.local>
2013-06-26 15:52:09 +0930

    now gluttonberg settings are moved into initialisers
    gluttonberg_basic_settings.rb
    gluttonberg_advance_settings.rb

---
+++ lib/generators/gluttonberg/installer/templates/gluttonberg_advance_settings.rb
@@ -0,0 +1,75 @@
+# Gluttonberg Advance config
+
+  # Asset Library Config
+    # Asset Storage
+      # asset storage by default set to :filesystem.
+      # :s3 is also an option. For s3 please make sure access key
+      # and secret code is provided in cms backend.
+      Rails.configuration.asset_storage = :filesystem
+    # Asset Processors
+      # apps/engines which depends on gluttonberg-core can
+      # use this to provide additional processor for assets
+      Rails.configuration.asset_processors = []
+    # Asset Mixin
+      # List of custom mixin for assets (It will enable developer to extend asset model)
+      Rails.configuration.asset_mixins = []
+
+  # Backend CSS and JS config
+    # setting it to true will include custom.css file in admin layout
+    # then you need to create custom.css or custom.sass file in you app
+    # purpose of this setting it allow front end developers customize backend per project
+    Rails.configuration.custom_css_for_cms = false
+
+    # setting it to true will include custom.js file in admin layout
+    # then you need to create custom.js file in you app
+    Rails.configuration.custom_js_for_cms = false
+
+  # Backend User Config
+    # User model always concat following three roles
+    # ["super_admin" , "admin" , "contributor"]
+    Rails.configuration.user_roles = []
+
+  # CMS based public stylesheets
+    Rails.configuration.cms_based_public_css = false
+
+  # Flagged Config
+    Rails.configuration.flagged_content = false
+
+  # Full text search config
+    Rails.configuration.search_models = {
+      "Gluttonberg::Page" => [:name],
+      "Gluttonberg::Blog" => [:name , :description],
+      "Gluttonberg::ArticleLocalization" => [:title , :body],
+      "Gluttonberg::PlainTextContentLocalization" => [:text] ,
+      "Gluttonberg::HtmlContentLocalization" => [:text]
+    }
+
+  # Honeypot field name
+    Rails.configuration.honeypot_field_name = "our_newly_weekly_series"
+
+  # Localization Config
+    Rails.configuration.localize = false
+    Rails.configuration.identify_locale = :prefix # currently thats the only option
+
+  # Membership Config
+    # Import Meta Data
+      # member import csv first row
+      Rails.configuration.member_csv_metadata = {
+        :first_name => "FIRST NAME",
+        :last_name => "LAST NAME",
+        :email => "EMAIL",
+        :groups => "GROUPS",
+        :bio => "BIO"
+      }
+    # Member Mixin
+      # List of custom mixin for member (It will enable to extend member model)
+      Rails.configuration.member_mixins = []
+
+    # Password pattern and validation message applies on both members and backend users
+    Rails.configuration.password_pattern = /^(?=.*\d)(?=.*[a-zA-Z])(?!.*[^\w\S\s]).{6,}$/
+    Rails.configuration.password_validation_message = "must be a minimum of 6 characters in length, contain at least 1 letter and at least 1 number"
+
+  # Multisite Config
+    # This setting needs to be provided in development.rb and/or production.rb
+    # config.multisite = {:site1 => "site1.com", :site2 => "site2.com" }
+    # config.multisite = {:site1 => "site1.local", :site2 => "site2.local" }
