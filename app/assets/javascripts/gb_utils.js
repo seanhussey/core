07a7c2bc00af3d98e1e0ba345ddf619c3256ccf4
Abdul Rauf <abdul@Abdul-Raufs-MacBook-Pro.local>
2013-07-02 15:23:00 +0930

    handleError for jquery

--- app/assets/javascripts/gb_utils.js
+++ app/assets/javascripts/gb_utils.js
@@ -25,6 +25,14 @@ Array.prototype.remove= function(){
 
 /* Jquery ajax file upload */
 jQuery.extend({
+  handleError: function( s, xhr, status, e ) {
+    // If a local callback was specified, fire it
+    if ( s.error )
+      s.error( xhr, status, e );
+    // If we have some XML response text (e.g. from an AJAX call) then log it in the console
+    else if(xhr.responseText)
+      console.log(xhr.responseText);
+  },
   createUploadIframe: function(id, uri)
 	{
 		//create frame
