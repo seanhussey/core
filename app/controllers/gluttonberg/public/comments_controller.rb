336b47740a2a13fc3683dbab6b4519de69ca4009
Abdul Rauf <abdul@Abdul-Raufs-MacBook-Pro.local>
2013-07-02 17:01:35 +0930

    find to where

--- app/controllers/gluttonberg/public/comments_controller.rb
+++ app/controllers/gluttonberg/public/comments_controller.rb
@@ -4,8 +4,8 @@ module Gluttonberg
       before_filter :is_blog_enabled
 
       def create
-        @blog = Gluttonberg::Blog.first(:conditions => {:slug => params[:blog_id]})
-        @article = Gluttonberg::Article.first(:conditions => {:slug => params[:article_id], :blog_id => @blog.id})
+        @blog = Gluttonberg::Blog.where(:slug => params[:blog_id]).first
+        @article = Gluttonberg::Article.where(:slug => params[:article_id], :blog_id => @blog.id).first
         @comment = @article.comments.new(params[:comment])
         @comment.blog_slug = params[:blog_id]
         @comment.author_id = current_member.id if current_member
@@ -16,7 +16,7 @@ module Gluttonberg
             end
           end
 
-          @subscription = CommentSubscription.find(:first , :conditions => {:article_id => @article.id , :author_email => @comment.writer_email })
+          @subscription = CommentSubscription.where(:article_id => @article.id , :author_email => @comment.writer_email).first
           if @comment.subscribe_to_comments == "1" && @subscription.blank?
             @subscription = CommentSubscription.create( {:article_id => @article.id , :author_email => @comment.writer_email , :author_name => @comment.writer_name } )
           elsif (@comment.subscribe_to_comments.blank? || @comment.subscribe_to_comments == "0")  && !@subscription.blank?
