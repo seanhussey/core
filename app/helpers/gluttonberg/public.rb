f207ecc908f51845a4a06744c2d5947f68e6a640
Abdul Rauf <abdul@Abdul-Raufs-MacBook-Pro.local>
2013-07-04 14:22:41 +0930

    more refactoring public helpers

--- app/helpers/gluttonberg/public.rb
+++ app/helpers/gluttonberg/public.rb
@@ -6,6 +6,7 @@ module Gluttonberg
       include PageInfo
       include MetaTags
       include NavTree
+      include HtmlTruncate
 
       # Returns the code for google analytics
       def google_analytics_js_tag
@@ -44,74 +45,6 @@ module Gluttonberg
           link_to "Inappropriate" , mark_as_flag_path(obj.class.name , obj.id)
         end
       end
-
-
-      # Does NOT behave identical to current Rails truncate method
-      # you must pass options as a hash not just values
-      # Sample usage: <%= html_truncate(category.description, :length =>
-      # 120, :omission => "(continued...)" ) -%>...
-
-      def html_truncate(html, truncate_length, options={})
-        text, result = [], []
-        previous_tags = []
-        # get all text (including punctuation) and tags and stick them in a hash
-        html.scan(/<\/?[^>]*>|[A-Za-z0-9.,\/&#;\!\+\(\)\-"'?]+/).each { |t| text << t }
-        #puts text
-        text.each do |str|
-          if truncate_length > 0
-            if str =~ /<\/?[^>]*>/
-              if str[0..1] != "</"
-                previous_tags.push(str)
-              else
-                previous_tags.pop()
-              end
-              result << str
-            else
-              result << str
-              truncate_length -= str.length
-            end
-          else
-            # now stick the next tag with that matches the previous
-            # open tag on the end of the result
-            while previous_tags.length > 0
-              previous_tag = previous_tags.pop()
-              unless previous_tag.start_with?("<br") || previous_tag.start_with?("<hr") || previous_tag.start_with?("<input")
-
-                tokens = previous_tag.split(" ")
-                if tokens.length == 1
-                  closing_tag = tokens.first.insert(1 , '/')
-                else
-                  closing_tag = "#{tokens.first.insert(1 , '/')}>"
-                end
-                result << closing_tag
-              end
-            end
-          end
-        end
-
-        return result.join(" ") + options[:omission].to_s
-      end
-
-
-      def db_stylesheet_link_tag
-        if Rails.configuration.cms_based_public_css == true
-           html = ""
-           Gluttonberg::Stylesheet.all.each do |stylesheet|
-             html << "\n"
-             unless stylesheet.css_prefix.blank?
-               html << stylesheet.css_prefix
-               html << "\n"
-             end
-             html << stylesheet_link_tag( stylesheets_path(stylesheet.slug) +".css?#{stylesheet.version}" )
-             unless stylesheet.css_postfix.blank?
-               html << "\n"
-               html << stylesheet.css_postfix
-             end
-           end
-           html << "\n"
-           html.html_safe
-         end
-       end
 
 
       def clean_public_query(string)
