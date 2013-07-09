20c50d044eb63ba563c4cec3e79748e6c47c02aa
Abdul Rauf <abdul@Abdul-Raufs-MacBook-Pro.local>
2013-07-09 14:42:32 +0930

    page localisation controller fix

--- app/controllers/gluttonberg/admin/content/page_localizations_controller.rb
+++ app/controllers/gluttonberg/admin/content/page_localizations_controller.rb
@@ -6,44 +6,20 @@ module Gluttonberg
         before_filter :authorize_user
 
         def edit
-
-          @page_localization.navigation_label = @page_localization.page.navigation_label if @page_localization.navigation_label.blank?
           @page = @page_localization.page
-           if(!(Gluttonberg.localized? && @page.localizations &&  @page.localizations.length > 1) )
-             @page_localization.slug = @page_localization.page.slug  if @page_localization.slug.blank?
-             @page_localization.save!
-           end
+          fix_nav_label_and_slug
           @version = params[:version]  unless params[:version].blank?
           prepare_to_edit
         end
 
         def update
-          # update localization updated_at value so that all contents have same version number.
-          @page_localization.contents.each do |content|
-            content.updated_at = Time.now
-          end
+          update_updated_at
           page_attributes = params["gluttonberg_page_localization"].delete(:page)
 
           if @page_localization.update_attributes(params["gluttonberg_page_localization"]) || !@page_localization.changed?
 
             @page_localization.page.update_attributes(page_attributes)
-
-            localization_detail = ""
-            if Gluttonberg.localized?
-              localization_detail = "(#{@page_localization.locale.slug})"
-            end
-
-            if params[:commit] && params[:commit] == "Publish" || params[:commit] && params[:commit] == "Update"
-              @page_localization.page.state = "published"
-              @page_localization.page.published_at = Time.now
-              @page_localization.page.save
-              Gluttonberg::Feed.log(current_user,@page_localization.page,"#{@page_localization.page.name} #{localization_detail}" , "updated and published.")
-            else
-              @page_localization.page.state = "draft"
-              @page_localization.page.published_at = nil
-              @page_localization.page.save
-              Gluttonberg::Feed.log(current_user,@page_localization.page,"#{@page_localization.page.name} #{localization_detail}" , "saved as draft.")
-            end
+            update_publish_state
 
             flash[:notice] = "The page was successfully updated."
             redirect_to edit_admin_page_page_localization_path( :page_id => params[:page_id], :id =>  @page_localization.id)
@@ -69,6 +45,39 @@ module Gluttonberg
             @descriptions = []
             Gluttonberg::PageDescription.all.each do |name, desc|
               @descriptions << [desc[:description], name]
+            end
+          end
+
+          def fix_nav_label_and_slug
+            @page_localization.navigation_label = @page.navigation_label if @page_localization.navigation_label.blank?
+            if(!(Gluttonberg.localized? && @page.localizations &&  @page.localizations.length > 1) )
+              @page_localization.slug = @page_localization.page.slug  if @page_localization.slug.blank?
+              @page_localization.save!
+            end
+          end
+
+          def update_updated_at
+            # update localization updated_at value so that all contents have same version number.
+            @page_localization.contents.each do |content|
+              content.updated_at = Time.now
+            end
+          end
+
+          def update_publish_state
+            localization_detail = ""
+            if Gluttonberg.localized?
+              localization_detail = "(#{@page_localization.locale.slug})"
+            end
+            if params[:commit] && params[:commit] == "Publish" || params[:commit] && params[:commit] == "Update"
+              @page_localization.page.state = "published"
+              @page_localization.page.published_at = Time.now
+              @page_localization.page.save
+              Gluttonberg::Feed.log(current_user,@page_localization.page,"#{@page_localization.page.name} #{localization_detail}" , "updated and published.")
+            else
+              @page_localization.page.state = "draft"
+              @page_localization.page.published_at = nil
+              @page_localization.page.save
+              Gluttonberg::Feed.log(current_user,@page_localization.page,"#{@page_localization.page.name} #{localization_detail}" , "saved as draft.")
             end
           end
 
