f2a93f68835fad8590a05efc37cf87b21c68665f
Abdul Rauf <abdul@Abdul-Raufs-MacBook-Pro.local>
2013-07-09 11:29:10 +0930

    datetime helper refactoring

--- app/helpers/gluttonberg/admin/form_builder.rb
+++ app/helpers/gluttonberg/admin/form_builder.rb
@@ -25,36 +25,30 @@ module ActionView
         date_field_html_opts["data-datepicker"] = "bsdatepicker"
         unique_field_name = "#{field_name}_#{Gluttonberg::Member.generateRandomString}"
         date_field_html_opts[:onblur] = "checkDateFormat(this,'.#{unique_field_name}_error');combine_datetime('#{unique_field_name}');"
-        if date_field_html_opts[:class].blank?
-          date_field_html_opts[:class] = "small span2 datefield"
 
-        else
-          date_field_html_opts[:class] += " small span2 datefield"
-        end
+        date_field_html_opts[:class] = "" if date_field_html_opts[:class].blank?
+        date_field_html_opts[:class] += " small span2 datefield"
 
-        if time_field_html_opts[:class].blank?
-          time_field_html_opts[:class] = " small span2 timefield"
-        else
-          time_field_html_opts[:class] += " small span2 timefield"
-        end
-        time_field_html_opts[:onblur] = "checkTimeFormat(this,'.#{unique_field_name}_error');combine_datetime('#{unique_field_name}')"
+        time_field_html_opts[:class] = "" if time_field_html_opts[:class].blank?
+        time_field_html_opts[:class] += " small span2 timefield"
 
-        html = ""
-        date = ""
-        time = ""
+        time_field_html_opts[:onblur] = date_field_html_opts[:onblur]
+
+        date = time = ""
+
         unless self.object.send(field_name).blank?
           date = self.object.send(field_name).strftime("%d/%m/%Y")
           time = self.object.send(field_name).strftime("%I:%M %p")
         end
-        html += text_field_tag("#{unique_field_name}_date" , date , date_field_html_opts )
-        html += " "
-        html += text_field_tag("#{unique_field_name}_time" , time , time_field_html_opts )
-        html += self.hidden_field("#{field_name}" ,  :class => "#{unique_field_name}")
-        html += "<span class='help-block'><span class='span2'>DD/MM/YYYY</span> <span class='span2'>HH:MM AM/PM</span></span>"
-        html += "<label class='error #{unique_field_name}_error'></label>"
-        html += "<div class='clear'></div>"
-        html += "<script type='text/javascript'>$(document).ready(function() { combine_datetime('#{unique_field_name}'); }); </script>"
-        html.html_safe
+
+        render :partial => "/gluttonberg/admin/shared/datetime_field", :locals => {
+          :date_field_html_opts => date_field_html_opts,
+          :time_field_html_opts => time_field_html_opts,
+          :date => date,
+          :time => time,
+          :form => self,
+          :unique_field_name => unique_field_name
+        }
       end
 
 
