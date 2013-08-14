ffc6fc216906a053da2788b4a4e76ec610a866d3
Abdul Rauf <abdul@Abdul-Raufs-MacBook-Pro.local>
2013-08-14 14:53:09 +0930

    asset library helpers code refactoring

--- app/helpers/gluttonberg/asset_library.rb
+++ app/helpers/gluttonberg/asset_library.rb
@@ -7,11 +7,7 @@ module Gluttonberg
       if Rails.configuration.asset_storage == :s3
         url = asset.url
       else
-        if Rails.env=="development"
-          url = "http://#{request.host}:#{request.port}/user_asset/#{asset.asset_hash[0..3]}/#{asset.id}"
-        else
-          url = "http://#{request.host}/user_asset/#{asset.asset_hash[0..3]}/#{asset.id}"
-        end
+        url = "http://#{request.host_with_port}/user_asset/#{asset.asset_hash[0..3]}/#{asset.id}"
         if opts[:thumb_name]
           url << "/#{opts[:thumb_name]}"
         end
@@ -25,13 +21,18 @@ module Gluttonberg
 
     def asset_tag_v2(asset , options = {} , thumbnail_type = nil)
       if !asset.blank? && asset.category == "image"
+        _prepare_options_for_asset_tag(asset , options , thumbnail_type)
+        tag("img" , options)
+      end
+    end
+
+    private
+      def _prepare_options_for_asset_tag(asset , options = {} , thumbnail_type = nil)
         options[:class] = (options[:class].blank? ? asset.name.sluglize : "#{options[:class]} #{asset.name.sluglize}" )
         options[:title] = asset.name  unless options.has_key?(:title)
         options[:alt] = asset.alt.blank? ? asset.name : asset.alt unless options.has_key?(:alt)
         options[:src] = asset.url_for(thumbnail_type)
-        tag("img" , options)
       end
-    end
 
 
   end # Assets
