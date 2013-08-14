244a6b5d5453d34a9503329456bb63b7f7391b06
Abdul Rauf <abdul@Abdul-Raufs-MacBook-Pro.local>
2013-08-14 15:50:50 +0930

    locale observer code refactored

--- app/observers/gluttonberg/locale_observer.rb
+++ app/observers/gluttonberg/locale_observer.rb
@@ -3,34 +3,19 @@ module Gluttonberg
     observe Locale
     
     def after_create(locale)   
-      pages = Page.all
-      
-      pages.each do |page|
+      Page.all.each do |page|
         #create localizations for all pages for new locale
-        new_localizations = []
-        new_localizations << page.localizations.create(
-          :name     => page.name,
-          :locale_id   => locale.id
-        )
+        new_localizations = create_page_localization(page, locale)
         
         # create content localizations
         unless page.description.sections.empty?
           Rails.logger.info("Generating stubbed content for all pages using new localizations")
           page.description.sections.each do |name, section|
             # Create the content
-              association = page.send(section[:type].to_s.pluralize)
-              content = association.find(:first , :conditions => {:section_name => name})
-              # Create each localization
-               if content && content.class.localized?
-                   new_localizations.each do |localization|
-                     content.localizations.create(:parent => content, :page_localization => localization)
-                   end
-              end
+            create_page_content(page, new_localizations, name, section)
           end
         end
-        
       end
-      
     end
 
       
@@ -39,7 +24,28 @@ module Gluttonberg
       existing_localization_ids = []
       remove_list = []
       new_localizations = []
-    end  
+    end 
+
+    private
+      def create_page_localization(page, locale)
+        new_localizations = []
+        new_localizations << page.localizations.create(
+          :name     => page.name,
+          :locale_id   => locale.id
+        )
+        new_localizations
+      end 
+
+      def create_page_content(page, new_localizations, name, section)
+        association = page.send(section[:type].to_s.pluralize)
+        content = association.where(:section_name => name).first
+        # Create each localization
+        if content && content.class.localized?
+          new_localizations.each do |localization|
+            content.localizations.create(:parent => content, :page_localization => localization)
+          end
+        end
+      end
         
   end
 end
