d8bbbf51bd5c89284ac2d07c78eeb69973d7ab5b
Abdul Rauf <abdul@Abdul-Raufs-MacBook-Pro.local>
2013-08-20 16:00:20 +0930

    notifier spec fix

--- app/mailer/gluttonberg/base_notifier.rb
+++ app/mailer/gluttonberg/base_notifier.rb
@@ -4,6 +4,15 @@ module Gluttonberg
     protected
       def setup_email
         site_title = Gluttonberg::Setting.get_setting("title")
+        setup_from
+
+        @subject     = site_title.blank? ? "" : "[#{site_title}] "
+        @sent_on     = Time.now
+        @content_type = "text/html"
+      end
+
+      def setup_from
+        site_title = Gluttonberg::Setting.get_setting("title")
         from_email = Gluttonberg::Setting.get_setting("from_email")
 
         @from = ""
@@ -11,10 +20,6 @@ module Gluttonberg
           @from  = site_title.blank? ? from_email : "#{site_title} <#{from_email}>"
         end
         self.class.default :from => @from
-
-        @subject     = site_title.blank? ? "" : "[#{site_title}] "
-        @sent_on     = Time.now
-        @content_type = "text/html"
       end
   end
 end
