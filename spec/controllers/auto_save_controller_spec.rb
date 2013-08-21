0fe24d4322bfb7f88a2fdfe7665dd3aeeab7a95f
Abdul Rauf <abdul@Abdul-Raufs-MacBook-Pro.local>
2013-08-21 09:55:31 +0930

    auto save specs

--- spec/controllers/auto_save_controller_spec.rb
+++ spec/controllers/auto_save_controller_spec.rb
@@ -16,7 +16,10 @@ module Gluttonberg
       @locale = Gluttonberg::Locale.generate_default_locale
       Gluttonberg::Setting.generate_common_settings
       @asset = create_image_asset
-      
+      @page = Page.create! :name => 'first name', :description_name => 'generic_page'
+      @page_localization = @page.current_localization
+      @search_options = {:model_name => @page_localization.class.name, :id => @page_localization.id}
+      @actual_content = prepare_content_data(@page_localization.contents, @asset)
     end
 
     after :all do
@@ -24,52 +27,29 @@ module Gluttonberg
     end
 
     describe "POST create" do
-      it "save draft" do
+      it "save draft , retreive. destroy" do
         controller.instance_variable_set(:@current_user, @current_user)
-        @page = Page.create! :name => 'first name', :description_name => 'generic_page'
-        page_localization = @page.current_localization
+        content_params = { :gluttonberg_page_localization => @actual_content}
         
-        actual_content = prepare_content_data(page_localization.contents, @asset)        
-        content_params = { :gluttonberg_page_localization => actual_content}
-        
-        post :create, {:model_name => page_localization.class.name, :id => page_localization.id}.merge(content_params)
+        post :create, @search_options.merge(content_params)
         expect(response.status).to eq(200)
         expect(response.body).to eq("\"OK\"")
-        auto_save = AutoSave.where({:auto_save_able_id => page_localization.id, :auto_save_able_type => page_localization.class.name}).first
+        auto_save = AutoSave.where({:auto_save_able_id => @page_localization.id, :auto_save_able_type => @page_localization.class.name}).first
         auto_save.should_not be_nil
-        auto_save.data.should eql(actual_content.to_json)
+        auto_save.data.should eql(@actual_content.to_json)
+
+        put :retreive_changes, @search_options
+        expect(response.status).to eq(200)
+        expect(response.body).to eql(@actual_content.to_json)
+
+        delete :destroy, @search_options
+        expect(response.status).to eq(200)
+        expect(response.body).to eq("\"OK\"")
+
+        auto_save = AutoSave.where({:auto_save_able_id => @page_localization.id, :auto_save_able_type => @page_localization.class.name}).first
+        auto_save.should be_nil
       end
     end
-
-    # describe "GET retrieve" do
-    #   it "retrieve data" do
-    #     controller.instance_variable_set(:@current_user, @current_user)
-    #     put :update, {:id => @staff_profile.id}
-    #     expect(response.status).to eq(200)
-    #     feed = Gluttonberg::Feed.last
-    #     feed.should_not be_nil
-    #     feed.feedable_type.should == "StaffProfile"
-    #     feed.title.should == "Nick"
-    #     feed.action_type.should == "updated"
-    #     feed.user.id.should == @current_user.id
-    #   end
-    # end
-
-    # describe "DELETE destroy" do
-    #   it "destroy draft" do
-    #     controller.instance_variable_set(:@current_user, @current_user)
-    #     delete :destroy, {:id => @staff_profile.id}
-    #     expect(response.status).to eq(200)
-    #     feed = Gluttonberg::Feed.last
-    #     feed.should_not be_nil
-    #     feed.feedable_type.should == "StaffProfile"
-    #     feed.title.should == "Nick"
-    #     feed.action_type.should == "deleted"
-    #     feed.user.id.should == @current_user.id
-    #     create_staff
-    #   end
-    # end
-
 
     private
         
