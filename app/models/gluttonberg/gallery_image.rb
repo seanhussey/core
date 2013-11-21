4ca45a9d74a089c61340c76669c3e89058e7f8c4
Abdul Rauf <abdul@Abdul-Raufs-MacBook-Pro.local>
2013-11-21 14:29:11 +1030

    do not allow empty gallery image/video

--- app/models/gluttonberg/gallery_image.rb
+++ app/models/gluttonberg/gallery_image.rb
@@ -5,5 +5,6 @@ module Gluttonberg
     belongs_to :asset  , :class_name => "Gluttonberg::Asset" , :foreign_key => "asset_id"
     is_drag_tree :scope => :gallery_id , :flat => true , :order => "position"
     attr_accessible :asset_id, :position, :caption, :credits, :link
+    validates :asset_id, presence: true
   end
 end
