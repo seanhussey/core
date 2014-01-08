52139898d64ef1afde0ca812cf395fc7da5353d7
Yuri Tománek <yuri@tomanek.com.au>
2014-01-08 14:40:43 +1030

    Check if table exists.

--- app/helpers/gluttonberg/public/page_info.rb
+++ app/helpers/gluttonberg/public/page_info.rb
@@ -66,10 +66,12 @@ module Gluttonberg
         if !page.blank?
          "page #{page.current_localization.slug.blank? ? page.slug : page.current_localization.slug} #{page.home? ? 'home' : ''}"
         else
-          object = find_current_object_for_meta_tags
-          unless object.blank?
-            class_name = (object.kind_of?(Gluttonberg::ArticleLocalization) ? 'post' : object.class.name.demodulize.downcase)
-            "#{class_name} #{object.slug}"
+          if Gluttonberg::Article.table_exists?
+            object = find_current_object_for_meta_tags
+            unless object.blank?
+              class_name = (object.kind_of?(Gluttonberg::ArticleLocalization) ? 'post' : object.class.name.demodulize.downcase)
+              "#{class_name} #{object.slug}"
+            end
           end
         end
       end
@@ -100,7 +102,7 @@ module Gluttonberg
           end
         end
 
-        
+
     end
   end
 end
