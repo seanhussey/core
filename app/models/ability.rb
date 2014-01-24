d0d7b5ada26a31a6313cc47218b205233be9f618
Abdul Rauf <abdul.rauf.au@gmail.com>
2014-01-24 15:43:53 +1030

    abilities for contributors

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,9 +1,6 @@
 class Ability
   include CanCan::Ability
-  
-  # commented code is left for example for hostapps. In hostapp developer can override this class for custom authorization requirements
   def initialize(user)
-    
     user ||= User.new # guest user (not logged in)
     can :manage, :all
     if user.super_admin?
@@ -12,39 +9,27 @@ class Ability
     elsif user.editor?
       restricted_features_for_editors
     else
-      restricted_features_for_editors
-      cannot :destroy , Gluttonberg::Asset
-      
-      #page roles
-      #cannot :manage , Gluttonberg::Page
-      cannot :change_home , Gluttonberg::Page
-      cannot :destroy , Gluttonberg::Page
-      cannot :publish , Gluttonberg::Page
-      cannot :reorder , Gluttonberg::Page
-      
-      #cannot :manage , Gluttonberg::Blog
-      cannot :publish , Gluttonberg::Blog
-      cannot :destroy , Gluttonberg::Blog
-      
-      #cannot :manage , Gluttonberg::Article
-      #cannot :publish , Gluttonberg::Article
-      #cannot :destroy , Gluttonberg::Article
-      
-      #cannot :manage , Gluttonberg::Comment
-      #cannot :moderate , Gluttonberg::Comment
+      restricted_features_for_contributors
     end
-    
   end
   
   def restricted_features_for_admin
-    cannot :manage , Gluttonberg::Locale
-    cannot :create_or_destroy , Gluttonberg::Setting
+    cannot :manage, Gluttonberg::Locale
+    cannot :create_or_destroy, Gluttonberg::Setting
   end
 
   def restricted_features_for_editors
     restricted_features_for_admin
-    cannot :manage , User
-    cannot :manage , Gluttonberg::Member
-    cannot :manage , Gluttonberg::Setting
+    cannot :manage, User
+    cannot :manage, Gluttonberg::Member
+    cannot :manage, Gluttonberg::Setting
+  end
+
+  def restricted_features_for_contributors
+    restricted_features_for_editors
+    cannot :publish, :all
+    cannot :destroy, :all
+    cannot :moderate, :all
+    cannot :reorder, :all
   end
 end
