86db4263cb52c368390dbac4d7bb37afd6a118fc
Abdul Rauf <abdul.rauf.au@gmail.com>
2014-02-21 15:21:16 +1030

    page model refactoring in progress

--- app/models/gluttonberg/page.rb
+++ app/models/gluttonberg/page.rb
@@ -7,6 +7,7 @@ module Gluttonberg
     include Content::SlugManagement
     include Content::PageFinder
     include Content::DefaultTemplateFile
+    include Content::PageDescriptionInfo
     self.slug_scope = :parent_id
 
     belongs_to :user
@@ -64,52 +65,6 @@ module Gluttonberg
         load_localization
       end
       @current_localization
-    end
-
-    def redirect_required?
-      self.description.redirection_required?
-    end
-
-    def redirect_url
-      self.description.redirect_url(self,{})
-    end
-
-    # Indicates if the page is used as a mount point for a public-facing
-    # controller, e.g. a blog, message board etc.
-    def rewrite_required?
-      self.description.rewrite_required?
-    end
-
-    # Takes a path and rewrites it to point at an alternate route. The idea
-    # being that this path points to a controller.
-    def generate_rewrite_path(path)
-      path.gsub(current_localization.path, self.description.rewrite_route)
-    end
-
-    # Returns the PageDescription associated with this page.
-    def description
-      @description = PageDescription[self.description_name.to_sym] if self.description_name
-      @description
-    end
-
-    # Returns the page_options set in the PageDescription
-    def page_options
-      @page_options = description.options[:page_options]
-      @page_options
-    end
-
-    # Returns the name of the view template specified for this page —
-    # determined via the associated PageDescription
-    def view
-      self.description if @description.blank?
-      @description[:view] if @description
-    end
-
-    # Returns the name of the layout template specified for this page —
-    # determined via the associated PageDescription
-    def layout
-      self.description if @description.blank?
-      @description[:layout] if @description
     end
 
     # Returns the localized navigation label, or falls back to the page for a
